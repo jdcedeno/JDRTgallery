4244aa107104c22c1951b97af23515fa1a85bb6d
JDCedeno <josedcedenocode@gmail.com>
2020-12-04 09:38:36 -0500

    added artwork images to site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import alaverg from './img/.ALAVERG.png';
+import alaverghand from './img/alaverghand.png';
+import antipathy from './img/antipathy.jpg';
+import colibri from './img/colibri.jpg';
+import couplestherapy from './img/couplestherapy.jpg';
+import gamabunta from './img/gamabunta.jpg';
+import giraffe from './img/giraffe.jpg';
+import jdc from './img/jdc.jpg';
+import jmc from './img/jmc.jpg';
+import mc from './img/mc.jpg';
+import monkeybusiness from './img/monkeybusiness.jpg';
+import motherbird from './img/motherbird.jpg';
+import surrealboat from './img/surrealboat.jpg';
+import toiletmeditation from './img/toiletmeditation.jpg';
+import zebra from './img/zebra.jpg';
 
 function App() {
   return (
     <div className="App">
-      <h1>JDRT's Website</h1>
+      <h1>JDRT's Gallery</h1>
+      <img src={alaverg} alt="jdrt new painting"/>
+      <img src={alaverghand} alt="jdrt old painting" />
+      <img src={antipathy} alt="jdrt old painting 2" />
+      <img src={colibri} alt="jdrt old painting 3" />
+      <img src={couplestherapy} alt="jdrt old painting 4" />
+      <img src={gamabunta} alt="jdrt old painting 5" />
+      <img src={giraffe} alt="jdrt old painting 6" />
+      <img src={jdc} alt="jdrt old painting 7" />
+      <img src={jmc} alt="jdrt old painting 8" />
+      <img src={mc} alt="jdrt old painting 9" />
+      <img src={monkeybusiness} alt="jdrt old painting 10" />
+      <img src={motherbird} alt="jdrt old painting 11" />
+      <img src={surrealboat} alt="jdrt old painting 12" />
+      <img src={toiletmeditation} alt="jdrt old painting 13" />
+      <img src={zebra} alt="jdrt old painting 14" />
     </div>
   );
 }
