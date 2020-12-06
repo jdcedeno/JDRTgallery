d6129cfbdb4d5eb7988772c124bcae8a1186278b
JDCedeno <josedcedenocode@gmail.com>
2020-12-05 21:57:23 -0500

    fix title, add some images to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import karat24 from './img/24KARAT.jpg';
+import alaverg from './img/ALAVERG.png';
+import antipathy from './img/antipathy.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>JDRT's Gallery</h1>
+      <h2>24 KARAT</h2>
+      <img src={karat24} alt="JDRT 24 karat art"/>
+      <h2>ALAVERG!</h2>
+      <img src={alaverg} alt="JDRT ALAVERG art"/>
+      <h2>ANTIPATHY</h2>
+      <img src={antipathy} alt="JDRT antipathy art"/>
     </div>
   );
 }
